690bbd41b7a4f9486400514bbd71662f452979e4
KRSBX <firdaus.sati51@gmail.com>
2022-09-04 17:24:29 +0700

    chore: add three in dep

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  mozIndexedDB: IDBFactory;
+  webkitIndexedDB: IDBFactory;
+  msIndexedDB: IDBFactory;
+}
+
+interface EventTarget {
+  result: any;
+}
